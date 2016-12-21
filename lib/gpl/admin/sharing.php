7392fa21cfd2db9c7f09adf77bbfc5929fc46fbb
JS Morisset <jsm@surniaulula.com>
2016-12-21 12:46:19 -0800

    v1.4.5-1

--- lib/gpl/admin/sharing.php
+++ lib/gpl/admin/sharing.php
@@ -27,7 +27,7 @@ if ( ! class_exists( 'WpssoRrssbGplAdminSharing' ) ) {
 			if ( $this->p->debug->enabled )
 				$this->p->debug->mark();
 
-			SucomUtil::add_before_key( $table_rows, 'plugin_verify_certs', array(
+			SucomUtil::add_before_key( $table_rows, 'plugin_cache_info', array(
 				'plugin_sharing_buttons_cache_exp' => $form->get_th_html( _x( 'Sharing Buttons Cache Expiry',
 					'option label', 'wpsso-rrssb' ), null, 'plugin_sharing_buttons_cache_exp' ).
 				'<td nowrap class="blank">'.$this->p->options['plugin_sharing_buttons_cache_exp'].' '.
