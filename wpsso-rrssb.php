e3bc5c2b184d81120d6eaef9938980300ff42f41
JS Morisset <jsm@surniaulula.com>
2017-01-11 13:12:00 -0800

    v1.4.7-1

--- wpsso-rrssb.php
+++ wpsso-rrssb.php
@@ -12,7 +12,7 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: WPSSO extension to add Ridiculously Responsive (SVG) Social Sharing Buttons in your content, excerpts, CSS sidebar, widget, shortcode, etc.
  * Requires At Least: 3.7
- * Tested Up To: 4.7
+ * Tested Up To: 4.7.1
  * Version: 1.4.7-1
  * 
  * Version Numbering Scheme: {major}.{minor}.{bugfix}-{stage}{level}
