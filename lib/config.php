dc3a8483c1cb177b828373c84aca3eb5b8d55e1f
JS Morisset <jsm@surniaulula.com>
2017-03-04 01:57:29 -0800

    v1.4.11-rc4

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoRrssbConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssorrssb' => array(
-					'version' => '1.4.11-rc3',	// plugin version
+					'version' => '1.4.11-rc4',	// plugin version
 					'opt_version' => '12',		// increment when changing default options
 					'short' => 'WPSSO RRSSB',	// short plugin name
 					'name' => 'WPSSO Ridiculously Responsive Social Sharing Buttons (WPSSO RRSSB)',
@@ -29,7 +29,7 @@ if ( ! class_exists( 'WpssoRrssbConfig' ) ) {
 					'req' => array(
 						'short' => 'WPSSO',
 						'name' => 'WordPress Social Sharing Optimization (WPSSO)',
-						'min_version' => '3.40.2-rc3',
+						'min_version' => '3.40.2-rc4',
 					),
 					'img' => array(
 						'icon_small' => 'images/icon-128x128.png',
