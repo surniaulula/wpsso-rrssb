be40daf2e81a38019c31532f3d7960f008e675d8
JS Morisset <jsm@surniaulula.com>
2017-04-22 09:30:57 +0200

    v1.4.17

--- wpsso-rrssb.php
+++ wpsso-rrssb.php
@@ -13,7 +13,7 @@
  * Description: WPSSO extension to add Ridiculously Responsive (SVG) Social Sharing Buttons in your content, excerpts, CSS sidebar, widget, shortcode, etc.
  * Requires At Least: 3.7
  * Tested Up To: 4.7.4
- * Version: 1.4.17-rc.2
+ * Version: 1.4.17
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
