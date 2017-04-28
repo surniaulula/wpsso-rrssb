c55ff22206f258a87109f4bafe26ceeb46c6f21d
JS Morisset <jsm@surniaulula.com>
2017-04-28 07:08:20 +0200

    v1.4.18-dev.3

--- lib/config.php
+++ lib/config.php
@@ -29,7 +29,7 @@ if ( ! class_exists( 'WpssoRrssbConfig' ) ) {
 					'req' => array(
 						'short' => 'WPSSO',
 						'name' => 'WordPress Social Sharing Optimization (WPSSO)',
-						'min_version' => '3.42.0-dev.3',
+						'min_version' => '3.42.0-dev.2',
 					),
 					'img' => array(
 						'icons' => array(
