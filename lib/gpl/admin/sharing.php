5b6e98c98ac663bd6a65f83537f3b9a8caa8c988
JS Morisset <jsm@surniaulula.com>
2018-02-14 09:30:42 -0600

    v1.5.5-rc.1

--- lib/gpl/admin/sharing.php
+++ lib/gpl/admin/sharing.php
@@ -35,7 +35,7 @@ if ( ! class_exists( 'WpssoRrssbGplAdminSharing' ) ) {
 
 			SucomUtil::add_before_key( $table_rows, 'plugin_show_purge_count', array(
 				'plugin_sharing_buttons_cache_exp' => $form->get_th_html( _x( 'Sharing Buttons HTML Cache Expiry',
-					'option label', 'wpsso-rrssb' ), null, 'plugin_sharing_buttons_cache_exp' ).
+					'option label', 'wpsso-rrssb' ), '', 'plugin_sharing_buttons_cache_exp' ).
 				'<td nowrap class="blank">'.$this->p->options['plugin_sharing_buttons_cache_exp'].' '.
 				_x( 'seconds (0 to disable)', 'option comment', 'wpsso-rrssb' ).'</td>'.
 				WpssoAdmin::get_option_site_use( 'plugin_sharing_buttons_cache_exp', $form, $network ),
@@ -56,7 +56,7 @@ if ( ! class_exists( 'WpssoRrssbGplAdminSharing' ) ) {
 				array_merge( array( '' => 'none' ), $this->p->cf['sharing']['force_prot'] ) ).'</td>';
 
 			$table_rows['plugin_sharing_buttons_cache_exp'] = $form->get_th_html( _x( 'Sharing Buttons HTML Cache Expiry',
-				'option label', 'wpsso-rrssb' ), null, 'plugin_sharing_buttons_cache_exp' ).
+				'option label', 'wpsso-rrssb' ), '', 'plugin_sharing_buttons_cache_exp' ).
 			'<td nowrap class="blank">'.$this->p->options['plugin_sharing_buttons_cache_exp'].' '.
 				_x( 'seconds (0 to disable)', 'option comment', 'wpsso-rrssb' ).'</td>';
 
@@ -76,96 +76,11 @@ if ( ! class_exists( 'WpssoRrssbGplAdminSharing' ) ) {
 				return $table_rows;	// abort
 			}
 
-			$size_info = SucomUtil::get_size_info( 'thumbnail' );
-			$title_caption = $this->p->page->get_caption( 'title', 0, $mod, true, false );
+			$thumb_size_info = SucomUtil::get_size_info( 'thumbnail' );
+			$def_cap_title = $this->p->page->get_caption( 'title', 0, $mod, true, false );
 
-			$table_rows[] = '<td colspan="3" align="center">'.
-				$this->p->msgs->get( 'pro-feature-msg', 
-					array( 'lca' => 'wpssorrssb' ) ).'</td>';
-
-			/**
-			 * Email
-			 */
-			$caption_len = $this->p->options['email_cap_len'];
-			$caption_text = $this->p->page->get_caption( 'excerpt', $caption_len, 
-				$mod, true, $this->p->options['email_cap_hashtags'], true, 'none' );
-
-			$form_rows['email_title'] = array(
-				'label' => _x( 'Email Subject', 'option label', 'wpsso-rrssb' ),
-				'th_class' => 'medium', 'tooltip' => 'post-email_title', 'td_class' => 'blank',
-				'content' => $form->get_no_input_value( $title_caption, 'wide' ),
-			);
-			$form_rows['email_desc'] = array(
-				'label' => _x( 'Email Message', 'option label', 'wpsso-rrssb' ),
-				'th_class' => 'medium', 'tooltip' => 'post-email_desc', 'td_class' => 'blank',
-				'content' => $form->get_no_textarea_value( $caption_text, '', '', $caption_len ),
-			);
-
-			/**
-			 * Twitter
-			 */
-			$caption_len = $this->p->rrssb_sharing->get_tweet_max_len();
-			$caption_text = $this->p->page->get_caption( 'title', $caption_len, 
-				$mod, true, $this->p->options['twitter_cap_hashtags'] );
-
-			$form_rows['twitter_desc'] = array(
-				'label' => _x( 'Tweet Text', 'option label', 'wpsso-rrssb' ),
-				'th_class' => 'medium', 'tooltip' => 'post-twitter_desc', 'td_class' => 'blank',
-				'content' => $form->get_no_textarea_value( $caption_text, '', '', $caption_len ),
-			);
-
-			/**
-			 * Pinterest
-			 */
-			$caption_len = $this->p->options['pin_cap_len'];
-			$caption_text = $this->p->page->get_caption( 'excerpt', $caption_len, $mod, true, $this->p->options['pin_cap_hashtags'] );
-			$force_regen = $this->p->util->is_force_regen( $mod, 'schema' );	// false by default
-			$media = $this->p->og->get_media_info( $this->p->cf['lca'].'-pinterest-button',
-				array( 'pid', 'img_url' ), $mod, 'schema' );	// $md_pre = 'schema'
-
-			if ( ! empty( $media['pid'] ) ) {
-				list(
-					$media['img_url'],
-					$img_width,
-					$img_height,
-					$img_cropped,
-					$img_pid
-				) = $this->p->media->get_attachment_image_src( $media['pid'], 'thumbnail', false, $force_regen ); 
-			}
-
-			$form_rows['pin_desc'] = array(
-				'label' => _x( 'Pinterest Caption', 'option label', 'wpsso-rrssb' ),
-				'th_class' => 'medium', 'tooltip' => 'post-pin_desc', 'td_class' => 'blank top',
-				'content' => $form->get_no_textarea_value( $caption_text, '', '', $caption_len ).
-					( empty( $media['img_url'] ) ? '' : '</td><td class="top thumb_preview">'.
-						'<img src="'.$media['img_url'].'" style="max-width:'.$size_info['width'].'px;">' ),
-			);
-
-			/**
-			 * Generic Title / Caption Input
-			 */
-			Foreach ( array(
-				'linkedin' => 'LinkedIn',
-				'reddit' => 'Reddit',
-				'tumblr' => 'Tumblr',
-			) as $opt_prefix => $name ) {
-
-				$caption_len = $this->p->options[$opt_prefix.'_cap_len'];
-				$caption_text = $this->p->page->get_caption( 'excerpt', $caption_len,
-					$mod, true, $this->p->options[$opt_prefix.'_cap_hashtags'] );
-
-				$form_rows[$opt_prefix.'_title'] = array(
-					'label' => sprintf( _x( '%s Title', 'option label', 'wpsso-rrssb' ), $name ),
-					'th_class' => 'medium', 'tooltip' => 'post-'.$opt_prefix.'_title', 'td_class' => 'blank',
-					'content' => $form->get_no_input_value( $title_caption, 'wide' ),
-				);
-
-				$form_rows[$opt_prefix.'_desc'] = array(
-					'label' => sprintf( _x( '%s Caption', 'option label', 'wpsso-rrssb' ), $name ),
-					'th_class' => 'medium', 'tooltip' => 'post-'.$opt_prefix.'_desc', 'td_class' => 'blank',
-					'content' => $form->get_no_textarea_value( $caption_text, '', '', $caption_len ),
-				);
-			}
+			$table_rows[] = '<td colspan="3" align="center">'.$this->p->msgs->get( 'pro-feature-msg', 
+				array( 'lca' => 'wpssorrssb' ) ).'</td>';
 
 			/**
 			 * Disable Buttons Checkbox
@@ -175,6 +90,91 @@ if ( ! class_exists( 'WpssoRrssbGplAdminSharing' ) ) {
 				'th_class' => 'medium', 'tooltip' => 'post-buttons_disabled', 'td_class' => 'blank',
 				'content' => $form->get_no_checkbox( 'buttons_disabled' ),
 			);
+
+			/**
+			 * Email
+			 */
+			$email_cap_len  = $this->p->options['email_cap_len'];
+			$email_cap_hash = $this->p->options['email_cap_hashtags'];
+			$email_cap_text = $this->p->page->get_caption( 'excerpt', $email_cap_len, $mod, true, $email_cap_hash, true, 'none' );
+
+			$form_rows['email_title'] = array(
+				'label' => _x( 'Email Subject', 'option label', 'wpsso-rrssb' ),
+				'th_class' => 'medium', 'tooltip' => 'post-email_title', 'td_class' => 'blank',
+				'content' => $form->get_no_input_value( $def_cap_title, 'wide' ),
+			);
+
+			$form_rows['email_desc'] = array(
+				'label' => _x( 'Email Message', 'option label', 'wpsso-rrssb' ),
+				'th_class' => 'medium', 'tooltip' => 'post-email_desc', 'td_class' => 'blank',
+				'content' => $form->get_no_textarea_value( $email_cap_text, '', '', $email_cap_len ),
+			);
+
+			/**
+			 * Twitter
+			 */
+			$twitter_cap_len  = $this->p->rrssb_sharing->get_tweet_max_len();
+			$twitter_cap_hash = $this->p->options['twitter_cap_hashtags'];
+			$twitter_cap_text = $this->p->page->get_caption( 'title', $twitter_cap_len, $mod, true, $twitter_cap_hash );
+
+			$form_rows['twitter_desc'] = array(
+				'label' => _x( 'Tweet Text', 'option label', 'wpsso-rrssb' ),
+				'th_class' => 'medium', 'tooltip' => 'post-twitter_desc', 'td_class' => 'blank',
+				'content' => $form->get_no_textarea_value( $twitter_cap_text, '', '', $twitter_cap_len ),
+			);
+
+			/**
+			 * Pinterest
+			 */
+			$pin_cap_len  = $this->p->options['pin_cap_len'];
+			$pin_cap_hash = $this->p->options['twitter_cap_hashtags'];
+			$pin_cap_text = $this->p->page->get_caption( 'excerpt', $pin_cap_len, $mod, true, $pin_cap_hash );
+			$pin_media    = $this->p->og->get_media_info( $this->p->lca.'-pinterest-button', array( 'pid', 'img_url' ), $mod, 'schema' );	// $md_pre = 'schema'
+			$force_regen  = $this->p->util->is_force_regen( $mod, 'schema' );	// false by default
+
+			if ( ! empty( $pin_media['pid'] ) ) {
+				list(
+					$pin_media['img_url'],
+					$img_width,
+					$img_height,
+					$img_cropped,
+					$img_pid
+				) = $this->p->media->get_attachment_image_src( $pin_media['pid'], 'thumbnail', false, $force_regen ); 
+			}
+
+			$form_rows['pin_desc'] = array(
+				'label' => _x( 'Pinterest Caption', 'option label', 'wpsso-rrssb' ),
+				'th_class' => 'medium', 'tooltip' => 'post-pin_desc', 'td_class' => 'blank top',
+				'content' => $form->get_no_textarea_value( $pin_cap_text, '', '', $pin_cap_len ).
+					( empty( $pin_media['img_url'] ) ? '' : '</td><td class="top thumb_preview">'.
+						'<img src="'.$pin_media['img_url'].'" style="max-width:'.$thumb_size_info['width'].'px;">' ),
+			);
+
+			/**
+			 * Other Title / Caption Input
+			 */
+			foreach ( array(
+				'linkedin' => 'LinkedIn',
+				'reddit' => 'Reddit',
+				'tumblr' => 'Tumblr',
+			) as $opt_prefix => $name ) {
+
+				$other_cap_len  = $this->p->options[$opt_prefix.'_cap_len'];
+				$other_cap_hash = $this->p->options[$opt_prefix.'_cap_hashtags'];
+				$other_cap_text = $this->p->page->get_caption( 'excerpt', $other_cap_len, $mod, true, $other_cap_hash );
+
+				$form_rows[$opt_prefix.'_title'] = array(
+					'label' => sprintf( _x( '%s Title', 'option label', 'wpsso-rrssb' ), $name ),
+					'th_class' => 'medium', 'tooltip' => 'post-'.$opt_prefix.'_title', 'td_class' => 'blank',
+					'content' => $form->get_no_input_value( $def_cap_title, 'wide' ),
+				);
+
+				$form_rows[$opt_prefix.'_desc'] = array(
+					'label' => sprintf( _x( '%s Caption', 'option label', 'wpsso-rrssb' ), $name ),
+					'th_class' => 'medium', 'tooltip' => 'post-'.$opt_prefix.'_desc', 'td_class' => 'blank',
+					'content' => $form->get_no_textarea_value( $other_cap_text, '', '', $other_cap_len ),
+				);
+			}
 
 			return $form->get_md_form_rows( $table_rows, $form_rows, $head, $mod );
 		}
