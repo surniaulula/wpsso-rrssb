c62d3f03ff239fa5a282eb8c208bb62b4156f125
JS Morisset <jsm@surniaulula.com>
2018-04-06 07:36:33 -0400

    v1.6.4

--- wpsso-rrssb.php
+++ wpsso-rrssb.php
@@ -200,12 +200,6 @@ if ( ! class_exists( 'WpssoRrssb' ) ) {
 					$this->p->notice->inf( $this->p->admin->get_check_for_updates_link() );
 				}
 			}
-
-			if ( method_exists( 'SucomUtil', 'safe_trigger_error' ) ) {
-				// translators: %s is the short plugin name
-				$error_prefix = sprintf( __( '%s warning:', 'wpsso-rrssb' ), $info['short'] );
-				SucomUtil::safe_trigger_error( $error_prefix.' '.rtrim( $error_msg, '.' ), E_USER_WARNING );
-			}
 		}
 	}
 
