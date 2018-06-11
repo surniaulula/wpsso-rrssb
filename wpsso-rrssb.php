9b357afdfb37cd32657aac476bb1b2c49b3d643c
JS Morisset <jsm@surniaulula.com>
2018-06-10 20:04:42 -0400

    v1.6.5

--- wpsso-rrssb.php
+++ wpsso-rrssb.php
@@ -70,9 +70,11 @@ if ( ! class_exists( 'WpssoRrssb' ) ) {
 		}
 
 		public static function &get_instance() {
+
 			if ( ! isset( self::$instance ) ) {
 				self::$instance = new self;
 			}
+
 			return self::$instance;
 		}
 
