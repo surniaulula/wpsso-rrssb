cc21851ec61b742078eb75d15a2a34a0b0c5c9d1
JS Morisset <jsm@surniaulula.com>
2018-12-15 17:45:59 -0600

    v1.13.0

---
+++ lib/gettext-pro.php
@@ -0,0 +1,21 @@
+<?php die( 'These aren\'t the droids you\'re looking for...' );
+
+/**
+ * Extract of translatable text strings from the lib/pro classes.
+ */
+_x( 'Sharing Buttons HTML Cache Expiry', 'option label', 'wpsso-rrssb' );
+_x( 'seconds (0 to disable)', 'option comment', 'wpsso-rrssb' );
+_x( 'Force Protocol for Shared URLs', 'option label', 'wpsso-rrssb' );
+_x( 'Sharing Buttons HTML Cache Expiry', 'option label', 'wpsso-rrssb' );
+_x( 'seconds (0 to disable)', 'option comment', 'wpsso-rrssb' );
+__( 'Save a draft version or publish the %s to display these options.', 'wpsso-rrssb' );
+_x( 'Disable Sharing Buttons', 'option label', 'wpsso-rrssb' );
+_x( 'Email Subject', 'option label', 'wpsso-rrssb' );
+_x( 'Email Message', 'option label', 'wpsso-rrssb' );
+_x( 'Tweet Text', 'option label', 'wpsso-rrssb' );
+_x( 'Pinterest Caption', 'option label', 'wpsso-rrssb' );
+_x( '%s Title', 'option label', 'wpsso-rrssb' );
+_x( '%s Caption', 'option label', 'wpsso-rrssb' );
+_x( 'Position in Woo Short Text', 'option label', 'wpsso-rrssb' );
+_x( 'Position in bbPress Single', 'option label', 'wpsso-rrssb' );
+_x( 'Position in BuddyBlog Post', 'option label', 'wpsso-rrssb' );
