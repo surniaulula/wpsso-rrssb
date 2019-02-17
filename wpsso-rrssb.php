b562366368f4eeb8982bd97df2ac6bb98a3c1c88
JS Morisset <jsm@surniaulula.com>
2019-02-16 19:04:36 -0600

    v1.14.0

--- wpsso-rrssb.php
+++ wpsso-rrssb.php
@@ -13,7 +13,7 @@
  * Description: WPSSO Core add-on offers Ridiculously Responsive (SVG) Social Sharing Buttons in your content, excerpts, CSS sidebar, widget, shortcode, editor pages, etc.
  * Requires PHP: 5.5
  * Requires At Least: 3.8
- * Tested Up To: 5.0
+ * Tested Up To: 5.1
  * WC Tested Up To: 3.5
  * Version: 1.14.0
  * 
