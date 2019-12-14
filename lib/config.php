17209e26798c83e5989629e14192a1d1d622675b
JS Morisset <jsm@surniaulula.com>
2019-12-14 09:29:11 -0800

    v3.1.0

--- lib/config.php
+++ lib/config.php
@@ -29,7 +29,7 @@ if ( ! class_exists( 'WpssoRrssbConfig' ) ) {
 					'req'         => array(
 						'short'       => 'WPSSO Core',
 						'name'        => 'WPSSO Core',
-						'min_version' => '6.15.0',
+						'min_version' => '6.16.0',
 					),
 					'assets' => array(
 						'icons' => array(
