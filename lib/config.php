c87be6eacf96fb4c6264622c2fa8807d21c67d1f
JS Morisset <jsm@surniaulula.com>
2020-05-03 08:18:13 -0700

    v4.2.0

--- lib/config.php
+++ lib/config.php
@@ -34,7 +34,7 @@ if ( ! class_exists( 'WpssoRrssbConfig' ) ) {
 						'wpsso' => array(
 							'class'       => 'Wpsso',
 							'name'        => 'WPSSO Core',
-							'min_version' => '7.2.0',
+							'min_version' => '7.3.0',
 						),
 					),
 
