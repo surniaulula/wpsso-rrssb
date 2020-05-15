c48c9d1b6494d9a12ab877f9467a12d136c98dc9
JS Morisset <jsm@surniaulula.com>
2020-05-15 10:36:38 -0700

    v4.3.0

--- lib/register.php
+++ lib/register.php
@@ -104,7 +104,7 @@ if ( ! class_exists( 'WpssoRrssbRegister' ) ) {
 				/**
 				 * Register plugin install, activation, update times.
 				 */
-				if ( class_exists( 'WpssoUtilReg' ) ) {	// Since WPSSO v6.13.1.
+				if ( class_exists( 'WpssoUtilReg' ) ) { // Since WPSSO Core v6.13.1.
 
 					$version = WpssoRrssbConfig::$cf[ 'plugin' ][ 'wpssorrssb' ][ 'version' ];
 
