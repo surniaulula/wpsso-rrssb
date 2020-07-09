a104e0e4017be324ac79e56bd34ed9517264cb7e
JS Morisset <jsm@surniaulula.com>
2020-07-09 08:58:12 -0700

    v4.4.2

--- wpsso-rrssb.php
+++ wpsso-rrssb.php
@@ -14,7 +14,7 @@
  * Requires PHP: 5.6
  * Requires At Least: 5.2
  * Tested Up To: 5.4.2
- * WC Tested Up To: 4.2.0
+ * WC Tested Up To: 4.3.0
  * Version: 4.4.2
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
