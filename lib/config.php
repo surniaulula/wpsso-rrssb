d23b26a2d7a28a15064e3f8ae8cf4d337dd57228
JS Morisset <jsm@surniaulula.com>
2020-08-01 09:34:39 -0700

    v4.5.0-dev.3

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoRrssbConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssorrssb' => array(			// Plugin acronym.
-					'version'     => '4.5.0-dev.2',	// Plugin version.
+					'version'     => '4.5.0-dev.3',	// Plugin version.
 					'opt_version' => '33',		// Increment when changing default option values.
 					'short'       => 'WPSSO RRSSB',	// Short plugin name.
 					'name'        => 'WPSSO Ridiculously Responsive Social Sharing Buttons',
@@ -42,7 +42,7 @@ if ( ! class_exists( 'WpssoRrssbConfig' ) ) {
 							'home'          => 'https://wordpress.org/plugins/wpsso/',
 							'plugin_class'  => 'Wpsso',
 							'version_const' => 'WPSSO_VERSION',
-							'min_version'   => '7.15.0-dev.2',
+							'min_version'   => '7.15.0-dev.3',
 						),
 					),
 
@@ -51,8 +51,8 @@ if ( ! class_exists( 'WpssoRrssbConfig' ) ) {
 					 */
 					'assets' => array(
 						'icons' => array(
-							'1x' => 'images/icon-128x128.png',
-							'2x' => 'images/icon-256x256.png',
+							'low'  => 'images/icon-128x128.png',
+							'high' => 'images/icon-256x256.png',
 						),
 					),
 
--- wpsso-rrssb.php
+++ wpsso-rrssb.php
@@ -15,7 +15,7 @@
  * Requires At Least: 5.2
  * Tested Up To: 5.5
  * WC Tested Up To: 4.3.1
- * Version: 4.5.0-dev.2
+ * Version: 4.5.0-dev.3
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
