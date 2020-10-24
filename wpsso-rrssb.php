210af17da5a2be3966f6a2dff80a9cbd5d0dbfcc
JS Morisset <jsm@surniaulula.com>
2020-10-24 10:31:11 -0700

    v5.2.0

--- wpsso-rrssb.php
+++ wpsso-rrssb.php
@@ -15,7 +15,7 @@
  * Requires At Least: 5.2
  * Tested Up To: 5.5.1
  * WC Tested Up To: 4.6.1
- * Version: 5.2.0-b.1
+ * Version: 5.2.0
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
