2beb054df3e415cd2426304fded7cf60a535cbe2
JS Morisset <jsm@surniaulula.com>
2020-12-22 09:14:56 -0800

    v5.5.1

--- lib/filters-upgrade.php
+++ lib/filters-upgrade.php
@@ -26,7 +26,7 @@ if ( ! class_exists( 'WpssoRrssbFiltersUpgrade' ) ) {
 			$this->a =& $addon;
 
 			$this->p->util->add_plugin_filters( $this, array( 
-				'rename_options_keys'  => 1,
+				'rename_options_keys' => 1,
 			) );
 		}
 
