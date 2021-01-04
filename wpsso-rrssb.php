6d42ef21cb1dfa20556255ea298409fc7c19c961
JS Morisset <jsm@surniaulula.com>
2021-01-04 14:13:03 -0800

    v5.6.0

--- wpsso-rrssb.php
+++ wpsso-rrssb.php
@@ -24,7 +24,7 @@
  *      {bugfix}        Backwards-compatible bug fixes or small improvements.
  *      {stage}.{level} Pre-production release: dev < a (alpha) < b (beta) < rc (release candidate).
  * 
- * Copyright 2014-2020 Jean-Sebastien Morisset (https://wpsso.com/)
+ * Copyright 2014-2021 Jean-Sebastien Morisset (https://wpsso.com/)
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
