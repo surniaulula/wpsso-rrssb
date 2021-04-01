f56387240da6b4ec22936132e18e8e4a07e30a77
JS Morisset <jsm@surniaulula.com>
2021-04-01 09:59:16 -0700

    v5.6.2

--- lib/config.php
+++ lib/config.php
@@ -43,7 +43,7 @@ if ( ! class_exists( 'WpssoRrssbConfig' ) ) {
 							'home'          => 'https://wordpress.org/plugins/wpsso/',
 							'plugin_class'  => 'Wpsso',
 							'version_const' => 'WPSSO_VERSION',
-							'min_version'   => '8.23.0',
+							'min_version'   => '8.25.2',
 						),
 					),
 
