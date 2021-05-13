7be96cebb157c580a5de2632702db04c57f22509
JS Morisset <jsm@surniaulula.com>
2021-05-12 20:08:50 -0700

    v5.7.1

--- wpsso-rrssb.php
+++ wpsso-rrssb.php
@@ -13,7 +13,7 @@
  * Description: Ridiculously Responsive (SVG) Social Sharing Buttons for your content, excerpts, CSS sidebar, widget, shortcode, templates, and editor.
  * Requires PHP: 7.0
  * Requires At Least: 5.2
- * Tested Up To: 5.7.1
+ * Tested Up To: 5.7.2
  * WC Tested Up To: 5.3.0
  * Version: 5.7.1
  * 
