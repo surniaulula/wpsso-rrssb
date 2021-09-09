bae8103a90eba240f4253f6e579ed18cd5234a15
JS Morisset <jsm@surniaulula.com>
2021-09-09 13:10:59 -0700

    v7.1.0-dev.3

--- lib/functions.php
+++ lib/functions.php
@@ -10,53 +10,31 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( 'These aren\'t the droids you\'re looking for.' );
 }
 
+/**
+ * $use_post can be true, false, or a post ID.
+ */
+if ( ! function_exists( 'wpssorrssb_get_post_sharing_buttons' ) ) {
+
+	function wpssorrssb_get_post_sharing_buttons( $use_post = true, $ids = array(), $atts = array() ) {
+
+		$wpsso =& Wpsso::get_instance();
+
+		if ( $wpsso->debug->enabled ) {
+
+			$wpsso->debug->log( 'use_post is ' . SucomUtil::get_use_post_string( $use_post ) );
+		}
+
+		$atts[ 'use_post' ] = $use_post;
+	
+		return wpssorrssb_get_sharing_buttons( $ids, $atts );
+	}
+}
+
 if ( ! function_exists( 'wpssorrssb_get_sharing_buttons' ) ) {
 
 	function wpssorrssb_get_sharing_buttons( $ids = array(), $atts = array() ) {
 
 		$wpsso =& Wpsso::get_instance();
-
-		$rrssb =& WpssoRrssb::get_instance();
-
-		if ( $wpsso->debug->enabled ) {
-
-			$wpsso->debug->mark();
-		}
-
-		$mtime_start = microtime( $get_float = true );
-
-		$error_msg = '';
-
-		if ( ! is_array( $ids ) ) {
-
-			$error_msg = 'sharing button ids must be an array';
-
-			error_log( __FUNCTION__ . '() error: ' . $error_msg );
-
-		} elseif ( ! is_array( $atts ) ) {
-
-			$error_msg = 'sharing button attributes must be an array';
-
-			error_log( __FUNCTION__ . '() error: ' . $error_msg );
-
-		} elseif ( ! $wpsso->avail[ 'p_ext' ][ 'rrssb' ] ) {
-
-			$error_msg = 'rrssb add-on is disabled';
-
-		} elseif ( empty( $ids ) ) {	// Nothing to do.
-
-			$error_msg = 'no buttons requested';
-		}
-
-		if ( ! empty( $error_msg ) ) {
-
-			if ( $wpsso->debug->enabled ) {
-
-				$wpsso->debug->log( 'exiting early: ' . $error_msg );
-			}
-
-			return '<!-- ' . __FUNCTION__ . ' exiting early: ' . $error_msg . ' -->' . "\n";
-		}
 
 		if ( $wpsso->debug->enabled ) {
 
@@ -67,6 +45,54 @@ if ( ! function_exists( 'wpssorrssb_get_sharing_buttons' ) ) {
 
 		$mod = $wpsso->page->get_mod( $atts[ 'use_post' ] );
 
+		return wpssorrssb_get_mod_sharing_buttons( $mod, $ids, $atts );
+	}
+}
+
+if ( ! function_exists( 'wpssorrssb_get_mod_sharing_buttons' ) ) {
+
+	function wpssorrssb_get_mod_sharing_buttons( array $mod, $ids = array(), $atts = array() ) {
+
+		$wpsso =& Wpsso::get_instance();
+
+		$rrssb =& WpssoRrssb::get_instance();
+
+		if ( $wpsso->debug->enabled ) {
+
+			$wpsso->debug->mark();
+		}
+
+		$error_msg = false;
+
+		if ( ! is_array( $ids ) ) {
+
+			$error_msg = 'ids must be an array';
+
+			error_log( __FUNCTION__ . '() error: ' . $error_msg );
+
+		} elseif ( ! is_array( $atts ) ) {
+
+			$error_msg = 'attributes must be an array';
+
+			error_log( __FUNCTION__ . '() error: ' . $error_msg );
+
+		} elseif ( empty( $ids ) ) {	// Nothing to do.
+
+			$error_msg = 'no buttons requested';
+		}
+
+		if ( $error_msg ) {
+
+			if ( $wpsso->debug->enabled ) {
+
+				$wpsso->debug->log( 'exiting early: ' . $error_msg );
+			}
+
+			return '<!-- ' . __FUNCTION__ . ' exiting early: ' . $error_msg . ' -->' . "\n";
+		}
+
+		$atts[ 'use_post' ] = false;	// Just in case.
+	
 		$buttons_html = $rrssb->social->get_html( $ids, $mod, $atts );
 
 		if ( $wpsso->debug->enabled ) {
@@ -77,17 +103,3 @@ if ( ! function_exists( 'wpssorrssb_get_sharing_buttons' ) ) {
 		return $buttons_html;
 	}
 }
-
-/**
- * $use_post can be true, false, or a post ID.
- */
-if ( ! function_exists( 'wpssorrssb_get_post_sharing_buttons' ) ) {
-
-	function wpssorrssb_get_post_sharing_buttons( $use_post = true, $ids = array(), $atts = array() ) {
-
-		$atts[ 'use_post' ] = $use_post;
-	
-		wpssorrssb_get_sharing_buttons( $ids, $atts );
-	}
-}
-
