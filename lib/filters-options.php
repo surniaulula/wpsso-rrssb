62785417aa09726e652e9f6db8650f3f02f12108
JS Morisset <jsm@surniaulula.com>
2021-09-15 13:26:32 -0700

    v7.1.0

--- lib/filters-options.php
+++ lib/filters-options.php
@@ -35,7 +35,7 @@ if ( ! class_exists( 'WpssoRrssbFiltersOptions' ) ) {
 
 		public function filter_option_type( $type, $base_key ) {
 
-			if ( ! empty( $type ) ) {
+			if ( ! empty( $type ) ) {	// Return early if we already have a type.
 
 				return $type;
 			}
