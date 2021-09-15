033c356bd6ec98c237c3e87e1c450f30b8392863
JS Morisset <jsm@surniaulula.com>
2021-09-15 14:24:36 -0700

    v7.1.0

--- lib/filters-options.php
+++ lib/filters-options.php
@@ -33,6 +33,9 @@ if ( ! class_exists( 'WpssoRrssbFiltersOptions' ) ) {
 			) );
 		}
 
+		/**
+		 * Return the sanitation type for a given option key.
+		 */
 		public function filter_option_type( $type, $base_key ) {
 
 			if ( ! empty( $type ) ) {	// Return early if we already have a type.
