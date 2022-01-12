c3047829be8325ae6e246af77fd2c5cb8eda6125
JS Morisset <jsm@surniaulula.com>
2022-01-12 07:01:40 -0800

    v8.2.0

--- wpsso-rrssb.php
+++ wpsso-rrssb.php
@@ -14,7 +14,7 @@
  * Requires PHP: 7.2
  * Requires At Least: 5.2
  * Tested Up To: 5.8.3
- * WC Tested Up To: 6.0.0
+ * WC Tested Up To: 6.1.0
  * Version: 8.2.0
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
