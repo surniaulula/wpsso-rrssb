2e03abdf9cff1275b958e7d562a00adb8e037710
JS Morisset <jsm@surniaulula.com>
2023-01-19 10:41:04 -0800

    v10.4.0-rc.2

--- lib/share/facebook.php
+++ lib/share/facebook.php
@@ -2,10 +2,11 @@
 /**
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl.txt
- * Copyright 2014-2023 Jean-Sebastien Morisset (https://wpsso.com/)
+ * Copyright 2015-2023 Jean-Sebastien Morisset (https://wpsso.com/)
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
+
 	die( 'These aren\'t the droids you\'re looking for.' );
 }
 
