7e1d6847bb6e21e708d33d27431b8d77cb032f6f
jsmoriss <jsm@surniaulula.com>
2024-01-25 20:53:08 +0000

    v11.1.0

--- wpsso-rrssb.php
+++ wpsso-rrssb.php
@@ -14,8 +14,8 @@
  * Requires Plugins: wpsso
  * Requires PHP: 7.2.34
  * Requires At Least: 5.8
- * Tested Up To: 6.4.2
- * WC Tested Up To: 8.5.1
+ * Tested Up To: 6.4.3
+ * WC Tested Up To: 8.5.2
  * Version: 11.1.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
