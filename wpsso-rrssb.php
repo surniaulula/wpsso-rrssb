2b2c25df4472bbb2cc5d695344e49dddcb2a3430
jsmoriss <jsm@surniaulula.com>
2024-02-03 16:05:23 +0000

    v11.2.0-dev.2

--- wpsso-rrssb.php
+++ wpsso-rrssb.php
@@ -16,7 +16,7 @@
  * Requires At Least: 5.8
  * Tested Up To: 6.4.3
  * WC Tested Up To: 8.5.2
- * Version: 11.2.0-dev.1
+ * Version: 11.2.0-dev.2
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
