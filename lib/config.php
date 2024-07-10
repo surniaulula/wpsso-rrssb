3d74436e107bca667387a14398ccd657a9a7287a
jsmoriss <jsm@surniaulula.com>
2024-07-10 16:43:13 +0000

    v11.4.0

--- lib/config.php
+++ lib/config.php
@@ -37,7 +37,7 @@ if ( ! class_exists( 'WpssoRrssbConfig' ) ) {
 							'home'          => 'https://wordpress.org/plugins/wpsso/',
 							'plugin_class'  => 'Wpsso',
 							'version_const' => 'WPSSO_VERSION',
-							'min_version'   => '17.17.0',
+							'min_version'   => '17.18.0',
 						),
 					),
 
