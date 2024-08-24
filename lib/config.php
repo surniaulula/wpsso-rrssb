1b24294b3c0a149ccaf6d20c14f3537305fe01b9
jsmoriss <jsm@surniaulula.com>
2024-08-24 17:00:19 +0000

    v11.7.0-dev.1

--- lib/config.php
+++ lib/config.php
@@ -37,7 +37,7 @@ if ( ! class_exists( 'WpssoRrssbConfig' ) ) {
 							'home'          => 'https://wordpress.org/plugins/wpsso/',
 							'plugin_class'  => 'Wpsso',
 							'version_const' => 'WPSSO_VERSION',
-							'min_version'   => '18.4.1-dev.1',
+							'min_version'   => '18.5.0-dev.1',
 						),
 					),
 
