fad6c61350711e34a882dcf0e0cb91bb777980ad
jsmoriss <jsm@surniaulula.com>
2025-04-09 15:21:40 +0000

    v11.7.0

--- lib/config.php
+++ lib/config.php
@@ -37,7 +37,7 @@ if ( ! class_exists( 'WpssoRrssbConfig' ) ) {
 							'home'          => 'https://wordpress.org/plugins/wpsso/',
 							'plugin_class'  => 'Wpsso',
 							'version_const' => 'WPSSO_VERSION',
-							'min_version'   => '18.19.5',
+							'min_version'   => '18.20.0',
 						),
 					),
 
